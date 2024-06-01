64dd7c71b18818d3cf29b953f493e21a2676a821
vi nguyen <nguyenduongthevi31415@gmail.com>
2024-06-01 19:54:47 +0700

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+        public IActionResult Index()
+
+        {
+
+
+            List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 999.99m },
+            new Product { Id = 2, Name = "Smartphone", Price = 699.99m },
+            new Product { Id = 3, Name = "Tablet", Price = 399.99m },
+            new Product { Id = 4, Name = "Monitor", Price = 199.99m },
+            new Product { Id = 5, Name = "Keyboard", Price = 49.99m },
+            new Product { Id = 6, Name = "Mouse", Price = 29.99m },
+            new Product { Id = 7, Name = "Printer", Price = 149.99m },
+            new Product { Id = 8, Name = "Webcam", Price = 89.99m },
+            new Product { Id = 9, Name = "Speaker", Price = 59.99m },
+            new Product { Id = 10, Name = "Headphones", Price = 79.99m }
+        };
+
+
+            /*ViewBag.productList = products;*/
+
+            return View(products);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult User(int id)
+        {
+            var user = new[]
+            {
+                new {Id=1, Name="Nghia"},
+                new {Id=2,  Name="Tu"},
+            };
+            string Name = null;
+            foreach (var item in user)
+            {
+                if(item.Id == id)
+                {
+                    Name = item.Name;
+                }
+            }
+            return Content(Name, "text/plain");
+        }
+      
+    }
+}
---
+++ WebApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Detail(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var Products = new[]
+     {
+        new { idP = 1, Name = "Nguyen Van A" },
+        new { idP = 2, Name = "Nguyen Van B" },
+        new { idP = 3, Name = "Nguyen Van C" }
+    };
+            string NameP = "";
+            foreach (var product in Products)
+            {
+                if(product.idP == id)
+                {
+                    NameP= product.Name;
+                }
+            }
+            ViewBag.ProName = NameP;
+            return View();
+        }
+
+    }
+}
+
